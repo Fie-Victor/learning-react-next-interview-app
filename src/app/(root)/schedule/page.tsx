6ebf512b6ed670d98dd6584c99d7c87dc1c95f71
Fiè Victor Traoré <fie-victor.traore@grenoble-inp.org>
2025-01-31 01:19:46 +0100

    Projet terminé SUivi dans le cadre de mon apprentissage de react, nextjs tailwind css, convex, stream. Le but de ce projet était de construire une platform d'interview pour des récruteus avant d'auditionner un candidat qui peut passer en direct des des interviews

---
+++ src/app/(root)/schedule/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { useUserRole } from "@/hooks/useUserRole";
+import { useRouter } from "next/navigation";
+import LoaderUI from "@/components/LoaderUI";
+import InterviewScheduleUI from "@/components/InterviewScheduleUI";
+function SchedulePage() {
+  const router = useRouter();
+  const { isInterviewer, isLoading } = useUserRole();
+
+  if(isLoading) return <LoaderUI />;
+  if(!isInterviewer) return router.push("/");
+
+  return <InterviewScheduleUI />;
+}
+
+export default SchedulePage;
---
+++ src/components/ui/Navbar.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { ModeToggle } from './ModeToggle'
+import { Code, CodeIcon } from 'lucide-react'
+import Link from 'next/link'
+import { SignedIn, UserButton } from '@clerk/nextjs'
+import { Sign } from 'crypto'
+import DashboardBtn from './DashboardBtn'
+
+function Navbar() {
+  return (
+    <nav className="border-b">
+        <div className='flex h-16 items-center px-4 container mx-auto'>
+        {/* LEFT SIDE -LOGO */}
+        <Link
+            href="/"
+            className="flex items-center gap-2 font-semibold text-2xl mr-6 font-mono hover:opacity-80
+            trasition-opacity"
+            >
+                <CodeIcon className="size-8 text-emerald-500" />
+                <span className="bg-gradient-to-r from-emerald-600 to-teal-500 bg-clip-text text-transparent">
+                    VicVideoCall
+                </span>
+            </Link>
+            {/* RIGHT SIDE - ACTIONS */}
+            
+            <SignedIn>
+                <div className='flex items-center space-x-4 ml-auto '>
+                    <DashboardBtn />
+                    <ModeToggle />
+                    <UserButton/>
+                </div>
+            </SignedIn>
+
+        </div>
+    </nav>
+  );
+}
+
+export default Navbar
